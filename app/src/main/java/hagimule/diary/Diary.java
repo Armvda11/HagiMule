d1d16b9a3015abe6015d879ee0640acd59ad0685
Armvda11 <hermasob@gmail.com>
2024-12-03 21:18:04 +0100

    Initial commit

---
+++ app/src/main/java/hagimule/diary/Diary.java
@@ -0,0 +1,43 @@
+package hagimule.diary;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+/**
+ * Interface for the Diary (Annuaire in French)
+ * Provides methods to register a file for a client and to get the files of a client
+ */
+public interface Diary extends Remote{
+
+    /**
+     * Method to register a file for a client
+     * 
+     * @param clientName client name
+     * @param fileName    file name
+     * @throws RemoteException if an error occurs during the remote communication
+     * @throws IllegalArgumentException if the filename is already registered
+     */
+    void registerClientFile(String clientName, String fileName) throws RemoteException , IllegalArgumentException;
+
+    /**
+     * Method to get the files of a client
+     * @param clientName client name
+     * @return the list of files of the client
+     * @throws RemoteException if an error occurs during the remote communication
+     */
+    List<String> getClientFiles(String clientName) throws RemoteException;
+
+
+    /**
+     * Method to remove a file for a client
+     * 
+     * @param clientName client name
+     * @param fileName    file name
+     * @return the file name removed
+     * @throws RemoteException if an error occurs during the remote communication
+     * @throws IllegalArgumentException if the filename is not registered
+     */
+    String removeClientFile(String clientName, String fileName) throws RemoteException, IllegalArgumentException;
+
+}
---
+++ app/src/main/java/hagimule/diary/DiaryImpl.java
@@ -0,0 +1,64 @@
+package hagimule.diary;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Implementation of the Diary interface
+ * Provides methods to register a file for a client and to get the files of a client
+ */
+public class DiaryImpl extends UnicastRemoteObject implements Diary {
+    // Map to store the files of each client
+    private Map<String, List<String>> clientFiles = new HashMap<>();
+
+    // Constructor
+    public DiaryImpl() throws RemoteException {
+        super();
+    }
+
+
+    // Implementation of the registerClientFile method
+    // !!! im not sure if the client could have multiple files
+    @Override
+    public void registerClientFile(String clientName, String fileName) throws RemoteException, IllegalArgumentException {
+        List<String> files = clientFiles.get(clientName);
+        // If the client does not have any files yet, create a new list
+        if (files == null) {
+            files = new ArrayList<>();
+            clientFiles.put(clientName, files);
+        }
+        // If the file is already registered, throw an exception
+        if (files.contains(fileName)) {
+            throw new IllegalArgumentException("File "+ fileName + " already registered for client "+ clientName);
+        }
+        files.add(fileName);
+    }
+
+    // implementation of the getClientFiles method
+    @Override
+    public List<String> getClientFiles(String clientName) throws RemoteException{
+        return clientFiles.get(clientName);
+    }
+
+
+    // Implementation of the removeClientFile method
+    @Override
+    public String removeClientFile(String clientName, String fileName) throws RemoteException, IllegalArgumentException {
+        List<String> files = clientFiles.get(clientName);
+        // If the client does not have any files yet, throw an exception
+        if (files == null) {
+            throw new IllegalArgumentException("Client"+clientName+ "not found");
+        }
+
+        // if the file is not registered, throw an exception
+        if (!files.contains(fileName)) {
+            throw new IllegalArgumentException("File "+ fileName + " not found for client "+ clientName);
+        }
+        files.remove(fileName);
+        return fileName;
+    }
+}
