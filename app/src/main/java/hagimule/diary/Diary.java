fa6380753be3335384911120a94768a5831b275d
Armvda11 <hermasob@gmail.com>
2024-12-04 09:14:38 +0100

    modif architecture

--- app/src/main/java/hagimule/diary/Diary.java
+++ app/src/main/java/hagimule/diary/Diary.java
@@ -6,9 +6,10 @@ import java.util.List;
 
 /**
  * Interface for the Diary (Annuaire in French)
- * Provides methods to register a file for a client and to get the files of a client
+ * Provides methods to register a file for a client, get the files of a client,
+ * remove a file for a client, and find clients who own a specific file.
  */
-public interface Diary extends Remote{
+public interface Diary extends Remote {
 
     /**
      * Method to register a file for a client
@@ -16,18 +17,18 @@ public interface Diary extends Remote{
      * @param clientName client name
      * @param fileName    file name
      * @throws RemoteException if an error occurs during the remote communication
-     * @throws IllegalArgumentException if the filename is already registered
+     * @throws IllegalArgumentException if the filename is already registered for the client
      */
-    void registerClientFile(String clientName, String fileName) throws RemoteException , IllegalArgumentException;
+    void registerClientFile(String clientName, String fileName) throws RemoteException, IllegalArgumentException;
 
     /**
-     * Method to get the files of a client
+     * Method to get the list of files for a client
+     * 
      * @param clientName client name
-     * @return the list of files of the client
+     * @return the list of files owned by the client
      * @throws RemoteException if an error occurs during the remote communication
      */
     List<String> getClientFiles(String clientName) throws RemoteException;
-
 
     /**
      * Method to remove a file for a client
@@ -36,8 +37,16 @@ public interface Diary extends Remote{
      * @param fileName    file name
      * @return the file name removed
      * @throws RemoteException if an error occurs during the remote communication
-     * @throws IllegalArgumentException if the filename is not registered
+     * @throws IllegalArgumentException if the filename is not registered for the client
      */
     String removeClientFile(String clientName, String fileName) throws RemoteException, IllegalArgumentException;
 
+    /**
+     * Method to find all clients who own a specific file
+     * 
+     * @param fileName the name of the file
+     * @return a list of client names who own the file
+     * @throws RemoteException if an error occurs during the remote communication
+     */
+    List<String> findClientsByFile(String fileName) throws RemoteException;
 }
--- app/src/main/java/hagimule/diary/DiaryImpl.java
+++ app/src/main/java/hagimule/diary/DiaryImpl.java
@@ -9,57 +9,113 @@ import java.util.Map;
 
 /**
  * Implementation of the Diary interface
- * Provides methods to register a file for a client and to get the files of a client
+ * Provides methods to register a file for a client, get the files of a client,
+ * remove a file for a client, and find clients by file.
  */
 public class DiaryImpl extends UnicastRemoteObject implements Diary {
-    // Map to store the files of each client
+    
+    // Map to store the files of each client: clientName -> List of files
     private Map<String, List<String>> clientFiles = new HashMap<>();
+    
+    // Map to store the clients who own a specific file: fileName -> List of client names
+    private Map<String, List<String>> fileOwners = new HashMap<>();
 
     // Constructor
     public DiaryImpl() throws RemoteException {
         super();
         this.clientFiles = new HashMap<>();
+        this.fileOwners = new HashMap<>();
     }
 
-
-    // Implementation of the registerClientFile method
-    // !!! im not sure if the client could have multiple files
+    /**
+     * Registers a file for a specific client.
+     * 
+     * @param clientName the name of the client
+     * @param fileName the name of the file to register
+     * @throws RemoteException if a remote exception occurs
+     * @throws IllegalArgumentException if the file is already registered for the client
+     */
     @Override
     public void registerClientFile(String clientName, String fileName) throws RemoteException, IllegalArgumentException {
+        // Get the current list of files for the client
         List<String> files = clientFiles.get(clientName);
+        
         // If the client does not have any files yet, create a new list
         if (files == null) {
             files = new ArrayList<>();
             clientFiles.put(clientName, files);
         }
+        
         // If the file is already registered, throw an exception
         if (files.contains(fileName)) {
-            throw new IllegalArgumentException("File "+ fileName + " already registered for client "+ clientName);
+            throw new IllegalArgumentException("File " + fileName + " already registered for client " + clientName);
         }
+        
+        // Register the file for the client
         files.add(fileName);
+
+        // Update the list of clients owning this file
+        fileOwners.computeIfAbsent(fileName, k -> new ArrayList<>()).add(clientName);
     }
 
-    // implementation of the getClientFiles method
+    /**
+     * Retrieves the list of files for a specific client.
+     * 
+     * @param clientName the name of the client
+     * @return a list of files owned by the client
+     * @throws RemoteException if a remote exception occurs
+     */
     @Override
-    public List<String> getClientFiles(String clientName) throws RemoteException{
-        return clientFiles.get(clientName);
+    public List<String> getClientFiles(String clientName) throws RemoteException {
+        return clientFiles.getOrDefault(clientName, new ArrayList<>());
     }
 
-
-    // Implementation of the removeClientFile method
+    /**
+     * Removes a file from a client's list of files.
+     * 
+     * @param clientName the name of the client
+     * @param fileName the name of the file to remove
+     * @return the name of the removed file
+     * @throws RemoteException if a remote exception occurs
+     * @throws IllegalArgumentException if the file is not registered for the client
+     */
     @Override
     public String removeClientFile(String clientName, String fileName) throws RemoteException, IllegalArgumentException {
         List<String> files = clientFiles.get(clientName);
+        
         // If the client does not have any files yet, throw an exception
         if (files == null) {
-            throw new IllegalArgumentException("Client"+clientName+ "not found");
+            throw new IllegalArgumentException("Client " + clientName + " not found");
+        }
+        
+        // If the file is not registered, throw an exception
+        if (!files.contains(fileName)) {
+            throw new IllegalArgumentException("File " + fileName + " not found for client " + clientName);
         }
 
-        // if the file is not registered, throw an exception
-        if (!files.contains(fileName)) {
-            throw new IllegalArgumentException("File "+ fileName + " not found for client "+ clientName);
-        }
+        // Remove the file from the client's list
         files.remove(fileName);
+        
+        // Also remove the client from the list of clients owning the file
+        List<String> owners = fileOwners.get(fileName);
+        if (owners != null) {
+            owners.remove(clientName);
+            if (owners.isEmpty()) {
+                fileOwners.remove(fileName);  // If no clients own the file, remove it from the map
+            }
+        }
+
         return fileName;
+    }
+
+    /**
+     * Finds all clients who own a specific file.
+     * 
+     * @param fileName the name of the file
+     * @return a list of client names who own the file
+     * @throws RemoteException if a remote exception occurs
+     */
+    public List<String> findClientsByFile(String fileName) throws RemoteException {
+        return fileOwners.getOrDefault(fileName, new ArrayList<>());
     }
 }
